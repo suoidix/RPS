98128f44f436540ed1bc4c44847e2e39e06adc5a
suoidix <130245469+suoidix@users.noreply.github.com>
2023-10-02 18:47:41 -0400

    Add function for player choice, add variables for scores

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,20 @@
 // Javascript for rock, paper, scissors game
 
-let rpsArr = ["Rock", "Paper", "Scissors"];
+let pcScore = 0;
+let cpuScore = 0;
+const choiceArr = ["rock", "paper", "scissors"];
 
+//function for computer selection
 function getComputerChoice() {
     return rpsArr[(Math.floor(Math.random() * rpsArr.length))]
+}
+
+//function for player choice
+function getPlayerChoice() {
+    const pcChoice = prompt("Choose: Rock, Paper, or Scissors:").toLowerCase(); // prompt for input and change case to lower
+    if (pcChoice == null || !choiceArr.includes(pcChoice)){ // determine if input is null or not equal to choice array
+        console.log("Please enter Rock, Paper, or Scissors."); // if true ask to input again
+    } else if (choiceArr.includes(pcChoice)) { // if present in array return choice
+        return pcChoice;
+    }
 }
