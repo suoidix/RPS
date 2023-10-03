07f13f60a42fe7dee0426087a9b8715c044dddda
suoidix <130245469+suoidix@users.noreply.github.com>
2023-10-02 22:30:55 -0400

    Fix bug with playerChoice returning undefiend after 1 wrong input
    Added return to line 17 prior to recursive function call

--- scripts/script.js
+++ scripts/script.js
@@ -12,11 +12,12 @@ function getComputerChoice() {
 //function for player choice
 function getPlayerChoice() {
     const pcChoice = prompt("Choose: Rock, Paper, or Scissors:").toLowerCase(); // prompt for input and change case to lower
-    if (pcChoice == null || !choiceArr.includes(pcChoice)){ // determine if input is null or not equal to choice array
+    if (pcChoice == null || !choiceArr.includes(pcChoice)) { // determine if input is null or not equal to choice array
         alert("Please enter Rock, Paper, or Scissors."); // if true ask to input again
-    } else if (choiceArr.includes(pcChoice)) { // if present in array return choice
-        return pcChoice;
-    }
+        return getPlayerChoice(); //must user return before function call, otherwise
+    } else if (choiceArr.includes(pcChoice)){ // if present in array return choice
+        return pcChoice; 
+    }  
 }
 
 //function for player name
@@ -26,7 +27,6 @@ function getPlayerName(){
 }
 
 //function for play a round
-//need to fix bug when player enters incorrect choice anything other than rock paper scissors and returns undefined
 function playRound() {
     const playerName = getPlayerName();
     const playerChoice = getPlayerChoice();
@@ -54,3 +54,5 @@ function playRound() {
         return `${playerName} wins! ${playerChoice} beats ${computerChoice}!`;
     } 
 }
+
+playRound()
