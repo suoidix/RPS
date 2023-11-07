55d968672dfdc1a1641a3a97c6a30a17a861ffad
suoidix <130245469+suoidix@users.noreply.github.com>
2023-11-07 16:08:15 -0500

    Remove code associated with rounds, end game by score of 5

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 // Javascript for rock, paper, scissors game
 const choiceArr = ["rock", "paper", "scissors"];
-let rounds = 5;
 let pcScore = 0;
 let cpuScore = 0;
 
@@ -21,15 +20,18 @@ function playRound(playerChoice) {
     let result = ""
 
     if (playerChoice === computerChoice) {
-        result = (`Tie! You both chose ${playerChoice}! <br><br> Player Score: ${pcScore} <br> Computer Score: ${cpuScore} <br> Computer Score: ${cpuScore} <br><br>Rounds Left: ${rounds - 1}`)
+        result = (`Tie! You both chose ${playerChoice}! 
+        <br><br> Player Score: ${pcScore} <br> Computer Score: ${cpuScore}`)
     }  else if (playerChoice === "rock" && computerChoice === "scissors"
     || playerChoice === "paper" && computerChoice === "rock"
     || playerChoice === "scissors" && computerChoice === "paper") {
         pcScore++;
-        result = (`You win, ${playerChoice} beats ${computerChoice}! <br><br> Player Score: ${pcScore} <br> Computer Score: ${cpuScore} <br> Computer Score: ${cpuScore} <br><br>Rounds Left: ${rounds - 1}`);
+        result = (`You win, ${playerChoice} beats ${computerChoice}! 
+        <br><br> Player Score: ${pcScore} <br> Computer Score: ${cpuScore}`);
     } else {
         cpuScore++;
-        result = (`You lose, ${computerChoice} beats ${playerChoice}! <br><br> Player Score: ${pcScore} <br> Computer Score: ${cpuScore} <br><br>Rounds Left: ${rounds - 1}`);
+        result = (`You lose, ${computerChoice} beats ${playerChoice}! 
+        <br><br> Player Score: ${pcScore} <br> Computer Score: ${cpuScore}`);
     }
     document.getElementById('result').innerHTML = result
 }
@@ -41,26 +43,21 @@ function getPlayerChoice() {
                 playRound(btn.value); 
                 console.log(`PC score: ${pcScore}`);
                 console.log(`CPU score: ${cpuScore}`);
-                rounds--;
-                if (pcScore > cpuScore && rounds === 0) { 
+                if (pcScore > cpuScore && pcScore === 5) { 
                     alert (`You win! You scored ${pcScore} points!`);
                     resetGame();
-                } else if (cpuScore > pcScore && rounds === 0) {
+                } else if (cpuScore > pcScore && cpuScore === 5) {
                     alert (`You lose! Computer scored ${cpuScore} points!`);
                     resetGame();
-                } else if (rounds === 0) {
-                    alert ('Tie! No one wins!')
-                    resetGame();
-                }
+                } 
         }) 
     })
 }
 
 //function to reset game
 function resetGame() {
-    result = `Game reset. <br><br>Rounds left: ${rounds}`
+    result = `Game reset. <br><br>Player Score: ${pcScore} <br> Computer Score: ${cpuScore}`
     document.getElementById('result').innerHTML = result
-    rounds = 5;
     pcScore = 0;
     cpuScore = 0;
 }
