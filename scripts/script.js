092dfba165b95c199ee90833a6c425d856790ed7
suoidix <130245469+suoidix@users.noreply.github.com>
2023-11-07 14:35:31 -0500

    Clean up old code

--- scripts/script.js
+++ scripts/script.js
@@ -14,24 +14,6 @@ function getComputerChoice() {
     return choiceArr[(Math.floor(Math.random() * choiceArr.length))]
 }
 
-//function for player choice
-/*function getPlayerChoice() {
-    const pcChoice = prompt("Choose: Rock, Paper, or Scissors:").toLowerCase(); // prompt for input and change case to lower
-    if (pcChoice == null || !choiceArr.includes(pcChoice)) { // determine if input is null or not equal to choice array
-        alert("Please enter Rock, Paper, or Scissors."); // if true ask to input again
-        return getPlayerChoice(); // must user return before recursive function call, otherwise otherwise the call will return undefined
-    } else if (choiceArr.includes(pcChoice)){ // if present in array return choice
-        return pcChoice; 
-    }  
-}*/
-
-
-/*function for player name
-function getPlayerName(){
-    const playerName = prompt("What is your name?")
-    return playerName; 
-}*/
-
 //function for play a round
 function playRound(playerChoice) {
 
@@ -63,7 +45,6 @@ function playRound(playerChoice) {
 }
 
 //function to play game
-
 function getPlayerChoice() {
     btns.forEach((btn) => {
         btn.addEventListener('click', () =>{
@@ -96,28 +77,3 @@ function getPlayerChoice() {
         }) 
     })
 }
-
-
-
-/*function game() {
-
-    alert("Let's play rock, paper, scissors!")
-
-    const playerName = 'Dave' //getPlayerName();
-    const playerChoice = getPlayerChoice();
-    playRound(playerName, playerChoice);
-
-    let rounds = 5;
-
-    for(let i = 0; i < rounds; i++) {
-        playRound(playerName);
-    }
-
-    //display winner based on best out of 5
-    if (pcScore > cpuScore) { 
-            alert (`You win! You scored ${pcScore} points!`);
-        } else if (cpuScore > pcScore) {
-            alert (`You lose! Computer scored ${cpuScore} points!`);
-        } else
-            alert ('Tie! No one wins!')  
-} */
