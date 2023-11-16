20c733a58126d852cf864c7c8df1dbf8b4a84f5a
suoidix <130245469+suoidix@users.noreply.github.com>
2023-11-16 11:13:58 -0500

    Create dynamic image for player/cpu choices

--- scripts/script.js
+++ scripts/script.js
@@ -19,11 +19,13 @@ function playRound(playerChoice) {
     const computerChoice = getComputerChoice();
     let result = ""
     let cpuItem = ""
+    let images = ""
 
     if (playerChoice === computerChoice) {
         result = (`Tie! You both chose ${playerChoice}! 
         <br><br> Player Score: ${pcScore} <br> Computer Score: ${cpuScore}`)
         cpuItem = (`Computer chose ${computerChoice}!<br><br>`)
+        images
     }  else if (playerChoice === "rock" && computerChoice === "scissors"
     || playerChoice === "paper" && computerChoice === "rock"
     || playerChoice === "scissors" && computerChoice === "paper") {
@@ -31,15 +33,17 @@ function playRound(playerChoice) {
         result = (`You win, ${playerChoice} beats ${computerChoice}! 
         <br><br> Player Score: ${pcScore} <br> Computer Score: ${cpuScore}`);
         cpuItem = (`Computer chose ${computerChoice}!<br><br>`)
+        images
     } else {
         cpuScore++;
         result = (`You lose, ${computerChoice} beats ${playerChoice}! 
         <br><br> Player Score: ${pcScore} <br> Computer Score: ${cpuScore}`);
         cpuItem = (`Computer chose ${computerChoice}!<br><br>`)
+        images
     }
     document.getElementById('result').innerHTML = result
     document.getElementById('cpuIcon').innerHTML = cpuItem
-
+    getImage(computerChoice, playerChoice);
 }
 
 //function to play game
@@ -50,22 +54,53 @@ function getPlayerChoice() {
                 console.log(`PC score: ${pcScore}`);
                 console.log(`CPU score: ${cpuScore}`);
                 if (pcScore === 5) { 
-                    alert (`You win! You scored ${pcScore} points!`);
+                    cpuItem = (`You win! You scored ${pcScore} points!`);
                     resetGame();
                 } else if (cpuScore === 5) {
-                    alert (`You lose! Computer scored ${cpuScore} points!`);
+                    cpuItem = (`You lose! Computer scored ${cpuScore} points!`);
                     resetGame();
                 } 
+                document.getElementById('cpuIcon').innerHTML = cpuItem
         }) 
     })
 }
 
 //function to reset game
 function resetGame() {
-    cpuItem = "<br><br>"
+    clearImage = ""
     result = `Game reset. <br><br><b>Final Score:</b><br>Player Score: ${pcScore} <br> Computer Score: ${cpuScore}`
     document.getElementById('result').innerHTML = result
-    document.getElementById('cpuIcon').innerHTML = cpuItem
+    document.getElementById('computerImage').innerHTML = clearImage
+    document.getElementById('playerImage').innerHTML = clearImage
     pcScore = 0;
     cpuScore = 0;
+}
+
+function getImage (computerChoice, playerChoice) {
+    let cpuImage = ""
+    let playerImage = ""
+
+    if (computerChoice === 'rock') {
+        cpuImage = '<img src ="./images/rock.png">'
+    }
+    else if (computerChoice === 'paper') {
+        cpuImage = '<img src ="./images/paper.png">'
+    }
+    else if (computerChoice === 'scissors') {
+        cpuImage = '<img src ="./images/scissors.png">'
+    }
+
+    if (playerChoice === 'rock') {
+        playerImage = '<img src ="./images/rock.png">'
+    }
+    else if (playerChoice === 'paper') {
+        playerImage = '<img src ="./images/paper.png">'
+    }
+    else if (playerChoice === 'scissors') {
+        playerImage = '<img src ="./images/scissors.png">'
+    }
+
+    document.getElementById('computerImage').innerHTML = cpuImage
+    document.getElementById('playerImage').innerHTML = playerImage
+
 }
